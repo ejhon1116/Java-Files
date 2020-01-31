3db6ccfeb430246479562f477183d75640aef1f5
ejhon1116 <ejhon1116@gmail.com>
2020-01-31 08:39:06 -0500

    Update Bank.java

--- Bank Java Files/Bank.java
+++ Bank Java Files/Bank.java
@@ -3,36 +3,32 @@ public class Bank {
     private Branch branch;
     
     public Bank() {
-        this.name = "";
-        this.branch= null;
+      this.name = "";
+      this.branch = null;
     }
 
     public Bank(String name, Branch branch) {
-        this.name = name;
-        this.branch = branch;
+      this.name = name;
+      this.branch = branch;
     }
     
-    public void setName(String newName)
-    {
-        this.name = newName;
+    public void setName(String newName) {
+      this.name = newName;
     }
 
-    public void setBranch(Branch newBranch)
-    {
-        this.branch = newBranch;
+    public void setBranch(Branch newBranch) {
+      this.branch = newBranch;
     }
 
-    public String getName()
-    {
-        return name;
+    public String getName() {
+      return name;
     }
 
-    public String getBranch()
-    {
-        return branch;
+    public String getBranch() {
+      return branch.toString();
     }
 
     public String toString() {
-        return "Name: " + name + " | Branch: " + branch;
+      return "Name: " + name + " | Branch: " + branch;
     }
 }
