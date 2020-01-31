89a6f0f505074bf6d2e727795651330585a99ca4
ejhon1116 <ejhon1116@gmail.com>
2020-01-31 08:39:51 -0500

    Update Branch.java

--- Bank Java Files/Branch.java
+++ Bank Java Files/Branch.java
@@ -2,51 +2,51 @@ public class Branch {
     private String address;
     private String number;
     private BankTeller bankTeller;
+    private static int count;
     
     public Branch()
     {
-        this.address = "";
-        this.number = "";
-        this.bankTeller = null;
+      this.address = "";
+      this.number = "";
+      count ++;
     }
 
     public Branch(String address, String number, BankTeller bankTeller) {
-        this.address = address;
-        this.number = number;
-        this.bankTeller = bankTeller;
+      this.address = address;
+      this.number = number;
+      this.bankTeller = bankTeller;
+      count ++;
     }
     
-    public void setAddress(String newAddress)
-    {
-        this.address = newAddress;
+    public void setAddress(String newAddress) {
+      this.address = newAddress;
     }
 
-    public void setNumber(String newNumber)
-    {
-        this.number = newNumber;
+    public void setNumber(String newNumber) {
+      this.number = newNumber;
     }
     
-    public void setTeller(BankTeller newTeller)
-    {
-        this.bankTeller = newTeller;
+    public void setTeller(BankTeller newTeller) {
+      this.bankTeller = newTeller;
     }
 
-    public String getAddress()
-    {
-        return address;
+    public String getAddress() {
+      return address;
     }
 
-    public String getNumber()
-    {
-        return number;
+    public String getNumber() {
+      return number;
     }
 
-    public String getTeller()
-    {
-        return bankTeller;
+    public String getTeller() {
+      return bankTeller.toString();
     }
-
+    
+    public static int getCount() {
+      return count;
+    }
+    
     public String toString() {
-        return "Address: " + address + " | Number: " + number + " | Bank Teller: " + bankTeller;
+      return "Address: " + address + " | Number: " + number + " | Bank Teller: " + bankTeller;
     }
 }
