a21243f42f6daffce5cb35d8a78d48b21d81b8fd
ejhon1116 <ejhon1116@gmail.com>
2020-01-31 08:39:36 -0500

    Update BankTeller.java

--- Bank Java Files/BankTeller.java
+++ Bank Java Files/BankTeller.java
@@ -3,64 +3,64 @@ import java.math.BigDecimal;
 public class BankTeller {
     private String name;
     private String ID;
-    private CustomerAccount account;
-    
+    private static int count = 0;
+      
     public BankTeller() {
-        this.name = "";
-        this.ID = "";
-        this.account = null;
-    }
-
-    public BankTeller(String name, String ID) {
-        this.name = name;
-        this.ID = ID;
-        this.account = null;
-    }
-
-    public BankTeller(String name, String ID, CustomerAccount account) {
-        this.name = name;
-        this.ID = ID;
-        this.account = account;
+      this.name = "";
+      this.ID = "";
+      count ++;
     }
     
+    public BankTeller(String name, String ID) {
+      this.name = name;
+      this.ID = ID;
+      count ++;
+    }
+    
+    public static int getCount() {
+      return count;
+    }
+        
     public void setName(String newName)
     {
-        this.name = newName;
+      this.name = newName;
     }
 
     public void setID(String newID)
     {
-        this.ID = newID;
-    }
-
-    public void setAccount(CustomerAccount newAccount)
-    {
-        this.account = newAccount;
+      this.ID = newID;
     }
 
     public String getName()
     {
-        return name;
+      return name;
     }
 
     public String getID()
     {
-        return ID;
-    }
-
-    public String getAccount()
-    {
-        return account;
+      return ID;
     }
     
-    public void addBalance(double deposit)
+    public void addBalance(CustomerAccount account, double deposit)
     {
-        double newbal = this.account.getBalance() + deposit;
-        BigDecimal rounded = new BigDecimal(newbal);
-        this.account.setBalance(rounded.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+      double newbal = account.getBalance() + deposit;
+      BigDecimal rounded = new BigDecimal(newbal);
+      account.setBalance(rounded.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
     }
 
+    public void withdrawBalance(CustomerAccount account, double withdrawal)
+    {
+      double newbal = account.getBalance() - withdrawal;
+      BigDecimal rounded = new BigDecimal(newbal);
+      account.setBalance(rounded.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+    }
+    
+    public double getBalance(CustomerAccount account)
+    {
+      return account.getBalance();
+    }
+    
     public String toString() {
-        return "Name: " + name + " | ID: " + ID;
+      return "Name: " + name + " | ID: " + ID;
     }
 }
