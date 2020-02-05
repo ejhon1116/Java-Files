13385892606dafe991528498d5ca7a22f96c3025
ejhon1116 <ejhon1116@gmail.com>
2020-02-05 08:39:29 -0500

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,18 +1,26 @@
 public class UserInfo {
   private String un;
-  private String pass;
+  private String enPass;
   
   public UserInfo (String un, String pass) {
     un = un;
-    pass = "";
+    enPass = encrypt(pass);
   }
   
   public void setUsername (String str) {
     un = str;
   }
   
-  public String getUsername (){
+  public String getUsername () {
     return un;
+  }
+  
+  public void setPassword(String newPass) {
+    enPass = encrypt(newPass);
+  }
+  
+  public String getPassword () {
+    return enPass;
   }
   
   public String removeWhitespaceAndConvertToUpper(String str) {
@@ -23,20 +31,20 @@ public class UserInfo {
     String temp = "";
     char[] arr = str.toCharArray();
     for(char e : arr) {
-      if(e == 'A') e = '@';
-      else if(e == 'E') e = '=';
-      else if(e == 'I') e = '!';
-      else if(e == 'J') e = '?';
-      else if(e == 'O') e = '*';
-      else if(e == 'P') e = '#';
-      else if(e == 'R') e = '&';
-      else if(e == 'S') e = '$';
-      else if(e == 'T') e = '+';
-      else if(e == 'V') e = '^';
-      else if(e == 'X') e = '%';
-      else if(e == ' ') e = '_';
+      if(e == 'A') temp = temp + '@';
+      else if(e == 'E') temp = temp + '=';
+      else if(e == 'I') temp = temp + '!';
+      else if(e == 'J') temp = temp + '?';
+      else if(e == 'O') temp = temp + '*';
+      else if(e == 'P') temp = temp + '#';
+      else if(e == 'R') temp = temp + '&';
+      else if(e == 'S') temp = temp + '$';
+      else if(e == 'T') temp = temp + '+';
+      else if(e == 'V') temp = temp + '^';
+      else if(e == 'X') temp = temp + '%';
+      else if(e == ' ') temp = temp + '_';
+      else temp = temp + e;
     }
-    for(char e : arr) temp = temp + e;
     return temp;
   }
   
@@ -49,7 +57,45 @@ public class UserInfo {
   }
   
   public String swapFirst2WithLast2 (String str) {
+    String tempstr = "";
     char[] arr = str.toCharArray();
     char[] temp = {arr[0], arr[1]};
+    arr[0] = arr[arr.length - 2];
+    arr[1] = arr[arr.length - 1];
+    arr[arr.length - 2] = arr[0];
+    arr[arr.length - 1] = arr[1];
+    for(char e : arr) tempstr = tempstr + e;
+    return tempstr;
+  }
+  
+  public String swapMiddleChars (String str) {
+    double length = str.length()/2;
+    int even = (int) length;
+    int odd = (int) (length + 0.5);
+    char[] arr = str.toCharArray();
+    String tempstr = "";
+    
+    if(str.length() % 2 == 1) {
+      char[] temp = {arr[odd - 1], arr[odd]};
+      arr[odd - 1] = arr[odd + 1];
+      arr[odd] = arr[odd + 2];
+      arr[odd + 1] = temp[0];
+      arr[odd + 2] = temp[1];
+      for(char e : arr) tempstr = tempstr + e;
+      return tempstr;
+    }
+    else {
+      char[] temp = {arr[even - 1], arr[even]};
+      arr[even - 1] = arr[even + 1];
+      arr[even] = arr[even + 2];
+      arr[even + 1] = temp[0];
+      arr[even + 2] = temp[1];
+      for(char e : arr) tempstr = tempstr + e;
+      return tempstr;
+    }
+  }
+  
+  public String encrypt (String str) {
+    return swapMiddleChars(swapFirst2WithLast2(swapHalfsforEncrypt(substitute(removeWhitespaceAndConvertToUpper(str)))));
   }
 }
