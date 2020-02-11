5906c4222cd790780103da9b8a2e4a00822de6d7
ejhon1116 <ejhon1116@gmail.com>
2020-02-11 08:38:45 -0500

    Update UserInfoDriver.java

--- Project #2/UserInfoDriver.java
+++ Project #2/UserInfoDriver.java
@@ -1,20 +1,25 @@
-import java.util.ArrayList;
+import java.util.*;
 import javax.swing.JOptionPane;
 import java.util.regex.Pattern;
 public class UserInfoDriver {
     public static void main(String[] args) {
+      List<String> names = new LinkedList<String>();
       boolean loop = true;
       while(loop == true) {
         loop = false;
-        ArrayList<UserInfo> users = new ArrayList<UserInfo>();
         String un = JOptionPane.showInputDialog(null, "Hello user! Please enter a username below.");
         String pass = JOptionPane.showInputDialog(null, "Please enter a password below.");
-        for (int i = 0; i < users.size(); i++) {
-          System.out.println(users.get(i).getUsername());
-          if(users.get(i).getUsername() == un) JOptionPane.showMessageDialog(null, "You suck.");
+        for(int i = 0; i < names.size(); i ++) {
+          if(names.get(i) == un) {
+            JOptionPane.showMessageDialog(null, "Another user has already chosen that username! Please pick another one!");
+            loop = true;
+          }
+          else {
+            names.add(un);
+            JOptionPane.showMessageDialog(null, "Username: " + un + "\nPassword: " + pass);
+            if(JOptionPane.showConfirmDialog(null, "Would you like to register a new user?") == 0) loop = true;
+          }
         }
-        users.add(new UserInfo(un, pass));
-        if(JOptionPane.showConfirmDialog(null, "Would you like to register a new user?") == 0) loop = true;
       }
     }
 }
